ac0012bf2a456fcbb625a48d3c0b67f845ffdc06
Ajeth S <ajethsakthivel@gmail.com>
2025-09-04 01:06:49 +0530

    Deployment fixes & added hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Dashboard from './Pages/Dashboard';
+import Dashboard from './Pages/Dashboard.jsx';
 import Employees from './Pages/employees.jsx';
 import HeaderSideNav from './Pages/Components/Header/HeaderSideNav.jsx';
 import './index.css';
@@ -11,14 +11,15 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <Router>
+    <HashRouter>
       <HeaderSideNav>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/employees" element={<Employees />} />
         </Routes>
       </HeaderSideNav>
-    </Router>
+    </HashRouter>
    </QueryClientProvider>
   </StrictMode>
 );
